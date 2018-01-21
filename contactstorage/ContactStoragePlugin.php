59af33516b4b22c0af66b5f529549dd80ef0f176
davidpanaho <davidpanaho@gmail.com>
2018-01-21 12:28:15 +0000

    Prepare 1.1.1 release

--- contactstorage/ContactStoragePlugin.php
+++ contactstorage/ContactStoragePlugin.php
@@ -19,7 +19,7 @@ class ContactStoragePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.1.0';
+        return '1.1.1';
     }
 
     public function getDeveloper()
